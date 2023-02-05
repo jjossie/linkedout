b95cb5c59f15e5c5200ac859626a5277ed5e8930
Joel Jossie <joeljossie@gmail.com>
2023-02-04 20:35:59 -0700

    put framework in place for t5-pa2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,45 @@
-import './App.css';
-import ProfileHeader from "./components/ProfileHeader";
-
 import 'react-icons';
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+
+
+// Project Imports
+import './App.css';
+
+import ProfileHeader from "./components/ProfileHeader";
 import PostPrompt from "./components/PostPrompt";
+import Main from "./components/top-level/Main";
+import {getConnectionRequests, getConnections, getFeed, getPosts} from "./services/loaders";
+import Profile from "./components/top-level/Profile";
+import User from "./components/top-level/User";
+import ConnectionRequests from "./components/top-level/ConnectionRequests";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    loader: getFeed,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+    loader: getConnections
+  },
+  {
+    path: "/user/:userId",
+    element: <User/>,
+    loader: getPosts
+  },
+  {
+    path: "/connectionRequests",
+    element: <ConnectionRequests/>,
+    loader: getConnectionRequests
+  }
+])
 
 function App() {
-  return (<>
-    <ProfileHeader/>
-    <PostPrompt/>
-  </>);
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
---
+++ frontend/src/services/loaders.js
@@ -0,0 +1,21 @@
+const loggedInUserId = "63dbfc0d187fe1e57908cfe1" // Carolyn Lindgren
+
+
+export async function getFeed(request) {
+
+}
+
+
+export async function getConnections(request) {
+
+}
+
+
+export async function getPosts(request) {
+
+}
+
+
+export async function getConnectionRequests(request) {
+
+}

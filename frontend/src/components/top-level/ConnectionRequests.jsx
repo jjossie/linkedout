71ab0bfed15324f8b2574c858c80bb8cc14779fe
Joel Jossie <joeljossie@gmail.com>
2023-03-18 12:27:29 -0600

    add a gap to ConnectionRequests

--- frontend/src/components/top-level/ConnectionRequests.jsx
+++ frontend/src/components/top-level/ConnectionRequests.jsx
@@ -5,6 +5,7 @@ import ConnectionRequest from "../ConnectionRequest";
 import MainPanelContainer from "../containers/MainPanelContainer";
 import CenterPanel from "../containers/CenterPanel";
 import LeftPanel from "../containers/LeftPanel";
+import Container from "@mui/material/Container";
 
 const ConnectionRequests = () => {
 
@@ -16,14 +17,20 @@ const ConnectionRequests = () => {
       <MainPanelContainer>
         <LeftPanel/>
         <CenterPanel>
-          {connectionRequests?.map?.(conReq => {
-            return <ConnectionRequest
-              firstName={conReq.firstName}
-              lastName={conReq.lastName}
-              userId={conReq.userId}
-              key={conReq.userId}
-            />
-          })}
+          <Container sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1em"
+          }}>
+            {connectionRequests?.map?.(conReq => {
+              return <ConnectionRequest
+                firstName={conReq.firstName}
+                lastName={conReq.lastName}
+                userId={conReq.userId}
+                key={conReq.userId}
+              />;
+            })}
+          </Container>
         </CenterPanel>
       </MainPanelContainer>
     </div>
